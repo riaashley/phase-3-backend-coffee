27182c397e980f3e74284c806be70ee10349cd5b
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 19:30:29 -0800

    coffee seeds

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+puts "Seeding Coffees..."
+
+# Seed your database here
+Coffee.create(coffee_name: "Caffe Americano", category: "hot", description: "Consists of an espresso shot diluted in hot water. Taste similar to black coffee", image_url: "https://images.pexels.com/photos/4195601/pexels-photo-4195601.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", shop_id: 1)
+Coffee.create(coffee_name: "Espresso", category: "hot", description: "Thicker than regular coffee and has a layer of crema on top", image_url: "https://images.pexels.com/photos/1660916/pexels-photo-1660916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", shop_id: 2)
+Coffee.create(coffee_name: "Macchiato", category: "hot", description: "Espresso-based drink with a small amount of foam on top", image_url: "https://images.pexels.com/photos/11712523/pexels-photo-11712523.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", shop_id: 3)
+Coffee.create(coffee_name: "Mocha", category: "hot", description: "A chocolate espresso drink with steamed milk and foam" , image_url: "https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", shop_id: 4)
+Coffee.create(coffee_name: "Flat White", category: "hot", description: "An espresso drink with steamed milk.", image_url: "https://images.pexels.com/photos/7810109/pexels-photo-7810109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", shop_id: 5)
+Coffee.create(coffee_name: "Irish", category: "hot", description: "Irish coffee consists of black coffee, whiskey and sugar, topped with whipped cream", image_url: "https://images.pexels.com/photos/10725922/pexels-photo-10725922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", shop_id: 1)
