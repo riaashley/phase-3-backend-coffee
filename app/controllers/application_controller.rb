87cf9655949a0b813d5888ca5d78320d4fbd5c9b
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 19:28:42 -0800

    change crud area

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,47 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  class ApplicationController < Sinatra::Base
+    set :default_content_type, 'application/json'
+  
+  #Get: list of Shops
+    get '/shops' do
+      shops = Shop.all
+      shops.to_json
+    end
+  
+  #Get: search shops by id
+    get '/shops/:id' do
+      shops = Shop.find(params[:id])
+      shops.to_json(include: :coffees)
+    end
+  
+  #Delete: removes shop by id
+    delete '/shops/:id' do
+      shops = Shop.find(params[:id])
+      shops.destroy
+      shops.to_json
+   end
+  
+  #Creates: adds a shop
+    post '/shops' do
+      shop = Shop.create(
+        name:params[:name], 
+        image_url:params[:image_url], 
+        location:params[:location], 
+        review:params[:review])
+      shop.to_json
+     end
+  
+     patch '/shops/:id' do
+      shop = Shop.find(params[:id])
+      shop.update(
+        name: params[:name],
+        image_url: params[:image_url],
+        location: params[:location],
+      )
+      shop.to_json
+    end
   end
 
 end
